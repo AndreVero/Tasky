46d2db01a9edee9757b1b1375ec8c123a4b555e1
AndreVero <andriyveremiyenko@gmail.com>
2023-07-22 18:17:11 +0300

    Fix issues with LoginE2E

--- app/src/main/java/com/vero/tasky/MainActivity.kt
+++ app/src/main/java/com/vero/tasky/MainActivity.kt
@@ -4,68 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Snackbar
-import androidx.compose.material.SnackbarHost
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.navigation.compose.rememberNavController
-import com.vero.tasky.core.presentation.components.LocalSnackbarHostState
-import com.vero.tasky.core.presentation.navigation.RootNavigation
+import com.vero.tasky.core.presentation.navigation.AppRoot
 import com.vero.tasky.ui.theme.TaskyTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    val viewModel: MainViewModel by viewModels()
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val state = viewModel.state
 
         installSplashScreen().apply {
-            setKeepOnScreenCondition { state.value.isLoading }
+            setKeepOnScreenCondition { viewModel.state.value.isLoading }
         }
 
         setContent {
             TaskyTheme {
-                val localSnackbarHostState = LocalSnackbarHostState.current
-                CompositionLocalProvider(
-                    LocalSnackbarHostState provides localSnackbarHostState
-                ) {
-
-                    LaunchedEffect(true) {
-                        viewModel.uiEvent.collect { errorMessage ->
-                            localSnackbarHostState.showSnackbar(getString(errorMessage))
-                        }
-                    }
-
-                    Scaffold(
-                        modifier = Modifier.fillMaxSize(),
-                        snackbarHost = {
-                            SnackbarHost(hostState = LocalSnackbarHostState.current) { data ->
-                                Snackbar(
-                                    backgroundColor = Color.Red,
-                                    snackbarData = data,
-                                    contentColor = Color.White
-                                )
-                            }
-                        },
-                    ) { paddingValues ->
-                        RootNavigation(
-                            navController = rememberNavController(),
-                            isLoggedIn = state.collectAsState().value.isLoggedIn,
-                            modifier = Modifier.padding(paddingValues)
-                        )
-                    }
-                }
+                AppRoot()
             }
         }
     }
---
+++ app/src/main/java/com/vero/tasky/core/presentation/navigation/AppRoot.kt
@@ -0,0 +1,57 @@
+package com.vero.tasky.core.presentation.navigation
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Snackbar
+import androidx.compose.material.SnackbarHost
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.rememberNavController
+import com.vero.tasky.MainViewModel
+import com.vero.tasky.core.presentation.components.LocalSnackbarHostState
+
+@Composable
+fun AppRoot() {
+    val context = LocalContext.current
+    val viewModel: MainViewModel = hiltViewModel()
+
+    val localSnackbarHostState = LocalSnackbarHostState.current
+    CompositionLocalProvider(
+        LocalSnackbarHostState provides localSnackbarHostState
+    ) {
+
+        LaunchedEffect(true) {
+            viewModel.uiEvent.collect { errorMessage ->
+                localSnackbarHostState.showSnackbar(context.getString(errorMessage))
+            }
+        }
+
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            snackbarHost = {
+                SnackbarHost(hostState = LocalSnackbarHostState.current) { data ->
+                    Snackbar(
+                        backgroundColor = Color.Red,
+                        snackbarData = data,
+                        contentColor = Color.White,
+                        modifier = Modifier.testTag("snackbar")
+                    )
+                }
+            },
+        ) { paddingValues ->
+            RootNavigation(
+                navController = rememberNavController(),
+                isLoggedIn = viewModel.state.collectAsState().value.isLoggedIn,
+                modifier = Modifier.padding(paddingValues)
+            )
+        }
+    }
+}
